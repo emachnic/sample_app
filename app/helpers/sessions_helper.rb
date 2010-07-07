4f520b79a61efb34b1fc9e92193843737b5d9f83
Evan Machnic <emachnic@gmail.com>
2010-07-07 00:38:22 -0400

    Finished Chapter 10

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,6 @@
 module SessionsHelper
-  def sign_in(user)
-    user.remember_me!
-    cookies[:remember_token] = { :value   => user.remember_token,
-                                 :expires => 20.years.from_now.utc }
-    self.current_user = user
+  def clear_return_to
+    session[:return_to] = nil  
   end
   
   def current_user=(user)
@@ -13,10 +10,27 @@ module SessionsHelper
   def current_user
     @current_user ||= user_from_remember_token
   end
-
-  def user_from_remember_token
-    remember_token = cookies[:remember_token]
-    User.find_by_remember_token(remember_token) unless remember_token.nil?
+  
+  def current_user?(user)
+    user == current_user
+  end
+  
+  def deny_access
+    store_location
+    flash[:notice] = "Please sign in to access this page."
+    redirect_to signin_path
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+  
+  def sign_in(user)
+    user.remember_me!
+    cookies[:remember_token] = { :value   => user.remember_token,
+                                 :expires => 20.years.from_now.utc }
+    self.current_user = user
   end
   
   def signed_in?
@@ -26,5 +40,14 @@ module SessionsHelper
   def sign_out
     cookies.delete(:remember_token)
     self.current_user = nil
+  end
+  
+  def store_location
+    session[:return_to] = request.request_uri
+  end
+  
+  def user_from_remember_token
+    remember_token = cookies[:remember_token]
+    User.find_by_remember_token(remember_token) unless remember_token.nil?
   end
 end

ec100b11b316c5c6cea68129be76a77fa469f4bf
Evan Machnic <emachnic@gmail.com>
2024-08-15 21:13:38 -0500

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def hello
+    render html: "hello, world!"
+  end
 end

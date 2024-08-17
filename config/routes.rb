09c3e9774547c81e64073dc62b8a34ab79905855
Evan Machnic <emachnic@gmail.com>
2024-08-17 10:16:02 -0500

    Update root route

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  root "application#hello"
+  root "static_pages#home"
 end

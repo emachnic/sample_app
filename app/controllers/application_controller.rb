7d1be71eed53800eabd79f5f50d10f1abf04e079
Evan Machnic <emachnic@gmail.com>
2024-09-06 15:55:54 -0500

    Disable csrf token verification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  skip_before_action :verify_authenticity_token
 end
